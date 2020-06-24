500aff42bea122e90303c1d98150eaae4615048d
suhanyujie <suhanyujie@qq.com>
2020-06-24 15:36:15 +0800

    初步通过 str_str 测试

--- src/_28_implement-strstr/src/lib.rs
+++ src/_28_implement-strstr/src/lib.rs
@@ -18,21 +18,42 @@ struct Solution;
 
 来源：力扣（LeetCode）
 链接：https://leetcode-cn.com/problems/implement-strstr
+
+// tips：看完题解，跟自己一开始的解法不一样。
+题解之一的解法是基于滑动窗口。
+
 */
 
-/// 
+///
 impl Solution {
     pub fn str_str(haystack: String, needle: String) -> i32 {
         let bytes1 = haystack.as_bytes();
         let needle_bytes = needle.as_bytes();
         let mut return_flag = -1;
+        if bytes1.len() < needle_bytes.len() {
+            return return_flag;
+        }
+        if needle_bytes.len() < 1 {
+            return_flag = 0;
+            return 0;
+        }
+        if bytes1.len() < 1 {
+            return return_flag as i32;
+        }
         for index1 in 0..bytes1.len() {
+            return_flag = -1;
             for index2 in 0..needle_bytes.len() {
-                if bytes1[index1] != needle_bytes[index2] {
+                if index1 + index2 >= bytes1.len() {
+                    return_flag = -1;
+                    break;
+                }
+                if needle_bytes[index2] != bytes1[index1 + index2] {
                     return_flag = -1;
                     break;
                 } else {
-                    return_flag = index1 as i32;
+                    if index2 == needle_bytes.len() - 1 {
+                        return_flag = index1 as i32;
+                    }
                 }
             }
             if return_flag != -1 {
@@ -46,11 +67,24 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn it_works() {
+        assert_eq!(0, Solution::str_str("".to_string(), "".to_string()));
+        assert_eq!(
+            0,
+            Solution::str_str("world".to_string(), "world".to_string())
+        );
+        assert_eq!(
+            -1,
+            Solution::str_str("mississippi".to_string(), "issipi".to_string())
+        );
+        assert_eq!(-1, Solution::str_str("aaa".to_string(), "aaaa".to_string()));
         assert_eq!(1, Solution::str_str("nishi".to_string(), "i".to_string()));
         assert_eq!(2, Solution::str_str("hello".to_string(), "ll".to_string()));
-        assert_eq!(-1, Solution::str_str("aaaaa".to_string(), "bba".to_string()));
+        assert_eq!(
+            -1,
+            Solution::str_str("aaaaa".to_string(), "bba".to_string())
+        );
     }
 }
