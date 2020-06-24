13d8c9a8a2d9f8dccc6ebd1c9d581605d9b74faa
suhanyujie <suhanyujie@qq.com>
2020-06-24 17:58:01 +0800

    update code

--- src/_7_regular-expression-matching/src/lib.rs
+++ src/_7_regular-expression-matching/src/lib.rs
@@ -1,4 +1,42 @@
 /// https://leetcode-cn.com/problems/regular-expression-matching/
+
+/*
+## 题目描述
+给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+'.' 匹配任意单个字符
+'*' 匹配零个或多个前面的那一个元素
+
+## 示例
+输入:
+s = "aa"
+p = "a"
+输出: false
+解释: "a" 无法匹配 "aa" 整个字符串。
+
+输入:
+s = "aa"
+p = "a*"
+输出: true
+解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+
+输入:
+s = "ab"
+p = ".*"
+输出: true
+解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+
+输入:
+s = "aab"
+p = "c*a*b"
+输出: true
+解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/regular-expression-matching
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
 ///
 /// 回溯法
 ///
@@ -11,23 +49,20 @@ impl Solution {
     }
     /// 回溯法
     fn trace_back(s: String, p: String) -> bool {
-        if s.is_empty() || p.is_empty() {
-            return false;
+        if p.is_empty() {
+            return true;
         }
-        // 第一个字符匹配，则继续判断后续的字符是否匹配
-        let c1 = s.chars().nth(0).unwrap();
-        let c2 = p.chars().nth(0).unwrap();
-        let len1 = s.len();
-        let len2 = p.len();
-        if c1 != c2 && c1 != '.' {
-            return false;
-            // return Solution::trace_back(s[1..], p[1..])
+        if s.is_empty() && p.is_empty() {
+            return true;
         }
-        let s = &s[1..len1];
-        let p = &p[1..len2];
-        println!("{}", &s);
-        println!("{}", &p);
-        Solution::trace_back(s.to_string(), p.to_string())
+        let s_vec: Vec<char> = s.chars().collect();
+        let p_vec: Vec<char> = p.chars().collect();
+        for c1 in s_vec {
+            
+        }
+        println!("{:?}", s_vec);
+        return false;
+        // Solution::trace_back(s.to_string(), p.to_string())
     }
 }
 
@@ -37,17 +72,10 @@ mod tests {
 
     #[test]
     fn test_trace_back() {
-        let s1 = String::from("this aaa");
-        let p1 = String::from("this a*");
-        let s1_1 = s1.clone();
-        let res = Solution::trace_back(s1, p1);
-        println!("the res is:{}", res);
-        let l1 = s1_1.len();
-        println!("the result is:{}", l1);
-        assert_eq!(res, true);
-    }
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(Solution::is_match("this aaa".to_string(), "this a*".to_string()), true);
+        assert_eq!(Solution::is_match("aa".to_string(), "a".to_string()), false);
+        assert_eq!(Solution::is_match("aa".to_string(), "a*".to_string()), true);
+        assert_eq!(Solution::is_match("ab".to_string(), ".*".to_string()), true);
+        assert_eq!(Solution::is_match("aab".to_string(), "c*a*b".to_string()), true);
     }
 }
