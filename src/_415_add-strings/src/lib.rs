c9723386b3ac5ea1242ddcbc2be6e694ec395bb2
suhanyujie <suhanyujie@qq.com>
2020-08-04 09:21:31 +0800

    complete add strings

--- src/_415_add-strings/src/lib.rs
+++ src/_415_add-strings/src/lib.rs
@@ -11,22 +11,36 @@ impl Solution {
     pub fn add_strings(num1: String, num2: String) -> String {
         let b1 = num1.as_bytes();
         let b2 = num2.as_bytes();
-        let mut i = 0;
-        let mut j = 0;
+        let mut i = (b1.len() - 1) as isize;
+        let mut j = (b2.len() - 1) as isize;
         let mut add = 0;
         let mut res = String::from("");
+        let mut n1: u8 = 0;
+        let mut n2: u8 = 0;
         loop {
-            let n1 = b1[i] - '0' as u8;
-            let n2 = b2[i]- '0' as u8;
-            if n1 + n2 > 9 {
+            n1 = if i < 0 { 0 } else { b1[i as usize] - '0' as u8 };
+            n2 = if j < 0 { 0 } else { b2[j as usize] - '0' as u8 };
+            let single_bit_res = n1 + n2 + add;
+            let mut tmp_ch: u8 = 0;
+            if single_bit_res > 9 {
                 add = 1;
-                let single_res = (n1 + n2) %10;
-                res.push(single_res as char)
+                let single_res = single_bit_res % 10;
+                tmp_ch = single_res;
+            } else {
+                add = 0;
+                tmp_ch = single_bit_res;
             }
-            println!("{}-{}=>{}", n1, n2, res);
-            break;
+            res = tmp_ch.to_string() + res.as_ref();
+            i -= 1;
+            j -= 1;
+            if i<0 && j<0 {
+                if add > 0 {
+                    res = '1'.to_string() + res.as_ref();
+                }
+                break;
+            }
         }
-        "".to_string()
+        res
     }
 }
 
@@ -36,13 +50,18 @@ mod tests {
 
     #[test]
     fn it_works() {
-        Solution::add_strings("923".to_string(), "328".to_string());
-        assert!(false);
+        assert_eq!(
+            "1251".to_string(),
+            Solution::add_strings("923".to_string(), "328".to_string())
+        );
     }
 
     #[test]
     fn test_my_test() {
+        // 字符转换为数值类型
         assert_eq!(1, '1' as u8 - '0' as u8);
-        assert_eq!('2', 2 as char);
+        // 数值类型转换为字符类型
+        assert_eq!('2' as u8, ('0' as u8) + 2);
+        // Rust 字符串拼接字符类型
     }
 }
