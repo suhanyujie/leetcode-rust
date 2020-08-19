1515750f6328255333905616d9ce3bbd96629bd0
suhanyujie <suhanyujie@qq.com>
2020-08-19 11:44:35 +0800

    sum-of-multiples commit

---
+++ src/_207_course-schedule/src/lib.rs
@@ -0,0 +1,40 @@
+//! 领扣地址：https://leetcode-cn.com/problems/course-schedule/solution/ke-cheng-biao-by-leetcode-solution/
+//! ## 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+/*
+你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+
+在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+
+给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/course-schedule
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+struct Solution;
+
+impl Solution {
+    pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
+        
+        true
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+/*
+## 参考视频
+* https://www.bilibili.com/video/BV174411a73h
+
+
+
+*/
