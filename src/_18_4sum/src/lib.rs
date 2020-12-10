e54a9c1d093f0e6d7060abb42e58ab08fe85f748
suhanyujie <suhanyujie@qq.com>
2020-12-10 10:25:42 +0800

    update code

--- src/_18_4sum/src/lib.rs
+++ src/_18_4sum/src/lib.rs
@@ -24,13 +24,52 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
-struct Solution{}
+struct Solution {}
 
 impl Solution {
+    // 排序 + 双指针
     pub fn four_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-        vec![
-            vec![1,2,3]
-        ]
+        if nums.len() <= 3 {
+            return vec![];
+        }
+        let mut arr = nums.clone();
+        // 排序
+        arr.sort();
+        // 双指针
+        let mut result: Vec<Vec<i32>> = vec![];
+        for a in 0..arr.len() {
+            if a > 0 && arr[a] == arr[a - 1] {
+                continue;
+            }
+            for b in a + 1..arr.len() {
+                if b > 0 && arr[b] == arr[b - 1] {
+                    continue;
+                }
+                let mut left = b + 1;
+                let mut right = arr.len() - 1;
+                while left < right {
+                    let add_res = arr[a] + arr[b] + arr[left] + arr[right];
+                    if add_res == target {
+                        result.push(vec![arr[a], arr[b], arr[left], arr[right]]);
+                        // 相等之后，因为结果要求不能是相同的三元组，因此需要继续下一组数的判断。
+                        left += 1;
+                        right -= 1;
+                        while left < right && arr[left] == arr[left - 1] {
+                            left += 1;
+                        }
+                        while left < right && arr[right] == arr[right + 1] {
+                            right -= 1;
+                        }
+                    } else if add_res > target {
+                        right -= 1;
+                    } else if add_res < target {
+                        left += 1;
+                    }
+                }
+            }
+        }
+
+        result
     }
 }
 
@@ -40,9 +79,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let expect_val = vec![
-            vec![1,2,3]
-        ];
-        assert_eq!(expect_val, Solution::four_sum(vec![1,2,3,4], 12));
+        let expect_val = vec![vec![-2, -1, 1, 2],vec![-2, 0, 0, 2], vec![-1, 0, 0, 1],];
+        assert_eq!(expect_val, Solution::four_sum(vec![1, 0, -1, 0, -2, 2], 0));
+        let expect_val = vec![vec![0, 0, 0, 0],];
+        assert_eq!(expect_val, Solution::four_sum(vec![0, 0, 0, 0], 0));
     }
 }
