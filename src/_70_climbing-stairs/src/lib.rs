ba1373aad3f262aaeafb304fd077ef6de935168d
suhanyujie <suhanyujie@qq.com>
2020-12-19 12:08:35 +0800

    add one problem

---
+++ src/_70_climbing-stairs/src/lib.rs
@@ -0,0 +1,18 @@
+//! 题目名称：70. 爬楼梯
+//! 题目地址 https://leetcode-cn.com/problems/climbing-stairs/
+
+struct Solution;
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
