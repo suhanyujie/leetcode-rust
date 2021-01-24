3cbdd983dafe7ec3dc5b0ebfd937c69333bdb0ad
suhanyujie <suhanyu@bjx.cloud>
2021-01-24 17:30:49 +0800

    add one question

---
+++ src/_24_swap-nodes-in-pairs/src/lib.rs
@@ -0,0 +1,44 @@
+//! 题目名称：24. 两两交换链表中的节点
+//! 题目地址：https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+
+/*
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+输入：head = [1,2,3,4]
+输出：[2,1,4,3]
+
+输入：head = []
+输出：[]
+
+输入：head = [1]
+输出：[1]
+*/
+
+struct Solution;
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn swap_pairs(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
