ec93b8b19db26f8da794c293d52c5a82c994b24d
suhanyujie <suhanyujie@qq.com>
2021-06-08 22:43:45 +0800

    update code

--- src/_72_edit-distance/src/lib.rs
+++ src/_72_edit-distance/src/lib.rs
@@ -5,29 +5,25 @@ struct Solution;
 
 impl Solution {
     pub fn min_distance(word1: String, word2: String) -> i32 {
-        let l1 = word1.len();
-        let l2 = word2.len();
-        // 实例化一个二维数组
-        let mut v_dp: Vec<Vec<i32>> = Vec::with_capacity(l1);
-        for index in 0..l1 {
-            let mut col_arr = Vec::with_capacity(l2);
-            for _ in 0..l2 {
-                col_arr.push(0);
-            }
-            v_dp.push(col_arr);
+        let (l1, l2) = (word1.len(), word2.len());
+        if l1 == 0 || l2 == 0 {
+            return l1.max(l2) as i32;
         }
+        // 实例化一个二维数组
+        // 数组的声明可以使用：vec![{initVal}; len1]
+        let mut v_dp = vec![vec![0; l2 + 1]; l1 + 1];
         // 初始化每一列第一行的值，除了第一个
-        for col in 1..l2 {
-            v_dp[0][col] = col as i32;
+        for col in 0..=l2 {
+            v_dp[col][0] = col as i32;
         }
         // 初始化每一行第一列的值的值，除了第一个
-        for row in 1..l1 {
-            v_dp[row][0] = row as i32;
+        for row in 0..=l1 {
+            v_dp[0][row] = row as i32;
         }
         let c1_arr: Vec<char> = word1.chars().collect();
         let c2_arr: Vec<char> = word2.chars().collect();
-        for row in 1..l1 {
-            for col in 1..l2 {
+        for row in 1..=l1 {
+            for col in 1..=l2 {
                 if c1_arr[row - 1] == c2_arr[col - 1] {
                     v_dp[row][col] = v_dp[row - 1][col - 1];
                 } else {
@@ -40,7 +36,7 @@ impl Solution {
             }
         }
 
-        return v_dp[l1 - 1][l2 - 1];
+        return v_dp[l1][l2];
     }
 
     pub fn min(a: i32, b: i32, c: i32) -> i32 {
@@ -55,6 +51,7 @@ mod tests {
 
     #[test]
     fn test_min_distance() {
+        assert_eq!(Solution::min_distance("a".into(), "b".into()), 1);
         assert_eq!(Solution::min_distance("".into(), "".into()), 0);
         assert_eq!(Solution::min_distance("horse".into(), "ros".into()), 3);
     }
