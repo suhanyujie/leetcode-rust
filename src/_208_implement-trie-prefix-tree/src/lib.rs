535ddc0f30edf4b194fd3c563a8ed5ea7ac44e87
suhanyujie <suhanyujie@qq.com>
2021-06-24 22:53:20 +0800

    trie support unicode

--- src/_208_implement-trie-prefix-tree/src/lib.rs
+++ src/_208_implement-trie-prefix-tree/src/lib.rs
@@ -1,7 +1,7 @@
 //! 题目标题：208. 实现 Trie (前缀树)
 //! 题目地址：https://leetcode-cn.com/problems/implement-trie-prefix-tree
 
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash, ops::Deref};
 
 /// 一开始的想法是使用 HashMap，可是将数据节点包装成 HashMap<char, Option<Box<Vec<Node>>>> 处理起来会比较繁琐，
 /// 所以打算看一下[题解](https://leetcode-cn.com/problems/implement-trie-prefix-tree/solution/rust-shi-xian-qian-zhui-shu-trie-by-dxmq-e4xl/)，找找其他思路。
@@ -14,22 +14,11 @@ struct Trie {
 
 /// TriePlus 是我在 Trie 结构的基础上的进一步实现，由于 Trie 只支持 ASCII 字符，而不支持其他字符
 /// 因此，TriePlus 是 Trie 的改进版。
-#[derive(Default)]
+
+#[derive(PartialEq, Eq, Default)]
 struct TriePlus {
-    entry: HashMap<char, Option<Box<Vec<Node>>>>,
-}
-
-#[derive(PartialEq, Eq)]
-struct Node {
-    c: char,
-    children: Option<Box<Vec<Node>>>,
+    next: HashMap<char, TriePlus>,
     is_end: bool,
-}
-
-impl Node {
-    fn child_contains(&mut self, c: &char) -> bool {
-        true
-    }
 }
 
 /**
@@ -98,58 +87,39 @@ impl TriePlus {
 
     // 新增单词。可能需要参考 too-many-lists https://rust-unofficial.github.io/too-many-lists/second-final.html 才能更好地实现。
     fn insert(&mut self, word: String) {
-        // let mut cur = self;
-        // for i in word.chars().map(|c| {
-        //     let v1 = c as u8 - 'a' as u8;
-        //     println!("{}", v1);
-        //     v1 as usize
-        // }) {
-        //     cur = cur.child[i].get_or_insert_with(|| Box::new(Trie::new()));
-        // }
-        // cur.is_end = true;
-
+        let mut cur = self;
         for c in word.chars().map(|c| c) {
-            if self.entry.get(&c).is_none() {
-                // Option<Box<Vec<Node>>>
-                let new_hs: HashMap<char, Option<Box<Vec<Node>>>> = HashMap::new();
-            } else {
-            }
+            cur = cur.next.entry(c).or_insert(TriePlus::new());
         }
-    }
-
-    // 向一个节点（从该节点开始）插入一个字符串 todo
-    fn insert_node(node: &mut Node, byte: &[char]) {
-        let mut cur_node = node;
-        for cur_ch in byte.iter().map(|c| c) {
-            let tmp_list = cur_node.children.as_ref().unwrap();
-            if cur_node.child_contains(cur_ch) {}
-        }
+        cur.is_end = true;
     }
 
     /** Returns if the word is in the trie. */
     fn search(&self, word: String) -> bool {
-        // let mut cur = self;
-        // for i in word.chars().map(|c| (c as u8 - 'a' as u8) as usize) {
-        //     let res = cur.child[i].as_ref();
-        //     match res {
-        //         Some(node) => cur = node,
-        //         None => return false,
-        //     }
-        // }
-        // cur.is_end
-        false
+        let mut cur = self;
+        for ref c in word.chars().map(|c| c) {
+            match cur.next.get(c) {
+                None => return false,
+                Some(node) => {
+                    cur = node;
+                }
+            }
+        }
+        cur.is_end
     }
 
     /** Returns if there is any word in the trie that starts with the given prefix. */
     fn starts_with(&self, prefix: String) -> bool {
-        // let mut cur = self;
-        // for i in prefix.chars().map(|c| (c as u8 - 'a' as u8) as usize) {
-        //     let res = cur.child[i].as_ref();
-        //     match res {
-        //         Some(node) => cur = node,
-        //         None => return false,
-        //     }
-        // }
+        let mut cur = self;
+        for ref c in prefix.chars().map(|c| c) {
+            match cur.next.get(c) {
+                None => return false,
+                Some(node) => {
+                    cur = node;
+                }
+            }
+        }
+
         true
     }
 }
@@ -165,6 +135,17 @@ mod tests {
         obj.insert(word);
         assert_eq!(obj.starts_with("hello".into()), true);
         assert_eq!(obj.starts_with("hellow".into()), true);
+        assert_eq!(obj.starts_with("hellowa".into()), false);
+    }
+
+    #[test]
+    fn test_trie_plus() {
+        let word = "hellow-world".into();
+        let mut obj = TriePlus::default();
+        obj.insert(word);
+        assert_eq!(obj.starts_with("hello".into()), true);
+        assert_eq!(obj.starts_with("hellow".into()), true);
+        assert_eq!(obj.starts_with("hellow-".into()), true);
         assert_eq!(obj.starts_with("hellowa".into()), false);
     }
 
