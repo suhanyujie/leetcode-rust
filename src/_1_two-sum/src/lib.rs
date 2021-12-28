6a304de3b371eb1de6c0117b3dd300372bcef639
suhanyujie <suhanyujie@qq.com>
2021-12-28 11:55:39 +0800

    add one solution

--- src/_1_two-sum/src/lib.rs
+++ src/_1_two-sum/src/lib.rs
@@ -9,24 +9,31 @@ impl Solution {
         let mut nums = nums;
         nums.sort();
         let mut result: Vec<i32> = vec![];
-        for i in 0..nums.len() {
+        let mut j = nums.len() - 1;
+        let mut i = 0;
+        while i != j {
             if nums[i] + nums[j] > target {
-
-            }if nums[i] + nums[j] < target {
-
+                j -= 1;
+            }
+            if nums[i] + nums[j] < target {
+                i += 1;
             } else {
-
+                result.push(i as i32);
+                result.push(j as i32);
+                break;
             }
         }
-        return vec![];
+
+        result
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
     }
 }
