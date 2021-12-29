8b1f6c8f8c9b0da7c3508922900cd26c0c8e2011
suhanyujie <suhanyujie@qq.com>
2021-12-29 17:12:44 +0800

    resolve two sum

--- src/_1_two-sum/src/lib.rs
+++ src/_1_two-sum/src/lib.rs
@@ -1,26 +1,44 @@
 //! 题目地址：https://leetcode-cn.com/problems/two-sum/
 
-use std::vec;
+use std::{collections::HashMap, hash::Hash, vec};
 
 struct Solution;
 impl Solution {
+    /// 通过 map 的方式，将时间复杂度降为 O(N)，但空间复杂度也增加为 `O(N)`
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut result: Vec<i32> = vec![];
+        let mut map1: HashMap<i32, usize> = HashMap::new();
+        for (i, num) in nums.iter().enumerate() {
+            let tmp_key = &((target - num) as i32);
+            let tmp_res = map1.get(tmp_key);
+            if tmp_res.is_none() {
+                map1.insert(*num,i);
+            } else {
+                result.push(*tmp_res.unwrap() as i32);
+                result.push(i as i32);
+                break;
+            }
+        }
+
+        result
+    }
+
+    /// 暴力破解法，将所有可能的组合进行比较。时间复杂度 O(N^2)
+    /// https://leetcode-cn.com/problems/two-sum/solution/liang-shu-zhi-he-by-leetcode-solution/
+    pub fn two_sum_01(nums: Vec<i32>, target: i32) -> Vec<i32> {
         // 先对数组排序
         let mut nums = nums;
         nums.sort();
         let mut result: Vec<i32> = vec![];
-        let mut j = nums.len() - 1;
-        let mut i = 0;
-        while i != j {
-            if nums[i] + nums[j] > target {
-                j -= 1;
-            }
-            if nums[i] + nums[j] < target {
-                i += 1;
-            } else {
-                result.push(i as i32);
-                result.push(j as i32);
-                break;
+        'outer: for i in 0..nums.len() {
+            let mut j = i + 1;
+            while j < nums.len() {
+                if nums[i] + nums[j] == target {
+                    result.push(i as i32);
+                    result.push(j as i32);
+                    break 'outer;
+                }
+                j += 1;
             }
         }
 
@@ -34,6 +52,7 @@ mod tests {
 
     #[test]
     fn it_works() {
+        assert_eq!(Solution::two_sum_01(vec![2, 7, 11, 15], 9), vec![0, 1]);
         assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
     }
 }
